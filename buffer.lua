a077d0886d272f392b9ac91ecf72243ee3879576
ocorreiododiogo <ocorreiododiogo@gmail.com>
2013-05-12 01:13:51 +0100

    change tabs to 4 spaces

--- buffer.lua
+++ buffer.lua
@@ -9,8 +9,8 @@ buffer.property['fold.by.indentation'] = '1'
 buffer.property['fold.line.comments'] = '0'
 
 -- Tabs and Indentation.
-buffer.tab_width = 2
-buffer.use_tabs = false
+buffer.tab_width = 4
+buffer.use_tabs = true
 --buffer.indent = 2
 buffer.tab_indents = true
 buffer.back_space_un_indents = true
